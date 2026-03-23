d09f4ad791b08d7e8acc7f05beabdcaa10f0b654
Henrik <henrik.andre@outlook.com>
2026-03-23 22:24:55 +0100

    Update fp-proxy.js

--- netlify/functions/fp-proxy.js
+++ netlify/functions/fp-proxy.js
@@ -45,7 +45,7 @@ exports.handler = async function(event) {
   // Hvilket endepunkt vil klienten ha?
   const endpoint = event.queryStringParameters?.endpoint || 'bank-deposits';
   const ALLOWED = {
-    'bank-deposits': '/feed/bank-deposits/all',
+    'bank-deposits': '/feed/deposits/all',
     'mortgages':     '/feed/mortgages/all',
     'banks':         '/feed/banks',
   };
